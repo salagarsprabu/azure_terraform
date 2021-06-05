a92f7187f3352210958cb35bbcc7883e1fa7a45d
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2021-06-05 21:34:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "salagars2022_AzTF"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "salagars_SBapp1"
 }
 
 variable "location" {
